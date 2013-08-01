0d3c25668cd24d170db73c1c3a43cb2246f7c45f
Rhubarb Sin <rhubarbsin@gmail.com>
2013-08-01 12:53:43 -0400

    convert net_get to gds

--- net_get.py
+++ net_get.py
@@ -2,10 +2,8 @@
 
 import sys
 import urllib2
-import logging
 from BaseHTTPServer import BaseHTTPRequestHandler
 
-import pyxb
 import ipaddr
 
 from apikey import APIKEY
@@ -16,11 +14,6 @@ if len(sys.argv) != 2:
     print 'Usage: %s CIDR' % sys.argv[0]
     sys.exit(2)
 
-log = logging.getLogger('pyxb.binding.content')
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-log.addHandler(ch)
-
 net = ipaddr.IPNetwork(sys.argv[1])
 url = 'http://whois.arin.net/rest/cidr/%s' % net
 try:
@@ -29,22 +22,22 @@ except urllib2.HTTPError as exc:
     print exc.code, BaseHTTPRequestHandler.responses[exc.code][1]
     sys.exit(1)
 else:
-    whoispayload = network.CreateFromDocument(response)
+    whoispayload = network.parseString(response)
 
-nethandle = whoispayload.handle[0]
+nethandle = whoispayload.handle
 print '''
 Net name: %s
 Net handle: %s
-''' % (whoispayload.name[0], nethandle)
+''' % (whoispayload.name, nethandle)
 url = 'https://reg.arin.net/rest/net/%s?apikey=%s' % (nethandle, APIKEY)
 try:
     response = urllib2.urlopen(url).read()
 except urllib2.HTTPError as exc:
-    errorpayload = ErrorPayload.CreateFromDocument(exc.read())
+    errorpayload = ErrorPayload.parseString(exc.read())
     print errorpayload.message[0]
     sys.exit(1)
 else:
-    netpayload = NetPayload.CreateFromDocument(response)
+    netpayload = NetPayload.parseString(response)
     print '''
 CIDR length: %s
 Address range: %s - %s
@@ -63,7 +56,3 @@ Description: %s
 
     # for poclink in netpayload.pocLinks:
     #     print poclink.pocLinkRef
-
-    netpayload.comment.append(pyxb.BIND(pyxb.BIND('Comment 1', number=1)))
-
-    # print netpayload.toDOM().toprettyxml()
