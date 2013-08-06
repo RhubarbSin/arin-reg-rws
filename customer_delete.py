043f9a543e74a010bad8182ee7b7422f9c8fa3dd
Rhubarb Sin <rhubarbsin@gmail.com>
2013-08-06 15:22:27 -0400

    add example for "delete customer"

---
+++ customer_delete.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+from StringIO import StringIO
+
+import requests
+
+from apikey import APIKEY
+from regrws import CustomerPayload, ErrorPayload
+
+if len(sys.argv) != 2:
+    print 'Usage: %s CUSTOMERHANDLE' % sys.argv[0]
+    sys.exit(2)
+
+custhandle = sys.argv[1]
+url = 'https://reg.arin.net/rest/customer/%s' % custhandle
+qargs = {'apikey': APIKEY}
+try:
+    r = requests.delete(url, params=qargs)
+except requests.exceptions.RequestException as e:
+    print 'ERROR:', e[0]
+    sys.exit(1)
+if r.status_code != requests.codes.ok:
+    errorpayload = ErrorPayload.parseString(r.content)
+    print r.status_code, errorpayload.message[0]
+    sys.exit(1)
+else:
+    custpayload = CustomerPayload.parseString(r.content)
