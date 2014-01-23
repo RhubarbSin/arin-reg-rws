29da71c23c68e18f18499f77bd85822e14aa094f
Rhubarb Sin <rhubarbsin@gmail.com>
2014-01-23 14:07:57 -0500

    move url definition into base class

--- regrws/method/net.py
+++ regrws/method/net.py
@@ -8,18 +8,15 @@ class _Method(restful.Method):
     """Base class for calls to NET-related RESTful methods."""
 
     payload = net  # payload module for this module's classes
+    url = 'https://reg.arin.net/rest/net/'
 
 class Get(_Method):
-
-    url = 'https://reg.arin.net/rest/net/'
 
     def __init__(self, session, handle):
         super(Get, self).__init__(session, 'get')
         self.url += handle
 
 class Delete(_Method):
-
-    url = 'https://reg.arin.net/rest/net/'
 
     def __init__(self, session, handle):
         super(Delete, self).__init__(session, 'delete')
